b8f8e6113b7c520afd2cbb0de12ab66c42336e01
Jalil S <jalil@example.com>
2024-01-03 09:20:29 -0500

    Added warp

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
+use warp::Filter;
 
 struct Question {
     id: QuestionId,
@@ -53,12 +54,24 @@ impl Question {
     }
 }
 
-fn main() {
+
+async fn get_questions() ->Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
-        QuestionId::from_str("1").expect("No ID Provided"),
-        "First Question".to_string(),
-        "Content of question".to_string(),
+        QuestionId::from_str("1").expect("No id Provided"),
+        "First question".to_string(),
+        "content of question".to_string(),
         Some(vec!["faq".to_string()]),
-    );
-    println!("{:#?}", question);
+  );
+
+    Ok(warp::reply::json(&question))
+}
+#[tokio::main]
+async fn main() {
+
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+    let routes = get_items;
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
