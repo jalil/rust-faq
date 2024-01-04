d4d69488c54673668287c15b92333184a9502401
Jalil S <jalil@example.com>
2024-01-04 05:29:07 -0500

    Added cors and custom error

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
-use warp::Filter;
+use warp::{http::Method, cors::CorsForbidden, http::StatusCode, reject::Reject, Filter, Rejection, Reply};
 
+#[derive(Debug)]
+struct InvalidId;
+
+impl Reject for InvalidId {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -10,37 +19,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-impl std::fmt::Display for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "{}, title: {}, content: {}, tags:{:?}",
-            self.id, self.title, self.content, self.tags
-        )
-    }
-}
-impl std::fmt::Display for QuestionId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "id: {}", self.0)
-    }
-}
-impl std::fmt::Debug for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "{:?}", self.tags)
-    }
-}
-
-impl FromStr for QuestionId {
-    type Err = std::io::Error;
-
-    fn from_str(id: &str) -> Result<Self, Self::Err> {
-        match id.is_empty() {
-            false => Ok(QuestionId(id.to_string())),
-            true => Err(Error::new(ErrorKind::InvalidInput, "No Id Provided")),
-        }
-    }
-}
-#[derive(Debug)]
+#[derive(Debug, Deserialize, PartialEq, Serialize, Clone, Eq, Hash)]
 struct QuestionId(String);
 
 impl Question {
@@ -54,24 +33,67 @@ impl Question {
     }
 }
 
-
-async fn get_questions() ->Result<impl warp::Reply, warp::Rejection> {
-    let question = Question::new(
-        QuestionId::from_str("1").expect("No id Provided"),
-        "First question".to_string(),
-        "content of question".to_string(),
-        Some(vec!["faq".to_string()]),
-  );
-
-    Ok(warp::reply::json(&question))
+#[derive(Debug, Deserialize, Clone)]
+struct Store {
+    questions: HashMap<QuestionId, Question>,
 }
+
+impl Store {
+    fn new() -> Self {
+        Self {
+            questions: HashMap::new(),
+        }
+    }
+
+    fn add_question(mut self, question: Question) -> Self {
+        self.questions.insert(question.id.clone(), question);
+        self
+    }
+
+    fn init() -> HashMap<QuestionId, Question> {
+        let file = include_str!("../questions.json");
+        serde_json::from_str(file).expect("cant read questions json file")
+    }
+}
+async fn get_questions(store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    let res: Vec<Question> = store.questions.values().cloned().collect();
+    Ok(warp::reply::json(&res))
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No Valid ID present".to_string(),
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    let store = Store::new();
+    let store_filter = warp::any().map(move || store.clone());
 
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
-        .and_then(get_questions);
+        .and(store_filter)
+        .and_then(get_questions)
+        .recover(return_error);
     let routes = get_items;
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
